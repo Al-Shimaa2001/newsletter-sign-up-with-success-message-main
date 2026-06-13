ad38e439bf45552646f328a1cd6ce0c920182e33
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2026-06-13 19:30:31 +0300

    add functionality for mail input and update some styles

---
+++ main.js
@@ -0,0 +1,24 @@
+let email = document.querySelector("#mail");
+let submit = document.querySelector(".submit");
+let error = document.querySelector(".error");
+submit.addEventListener("click", (e) => {
+  let value = email.value.trim();
+  if (value === "") {
+    e.preventDefault();
+    error.style.visibility = "visible";
+
+    errorVisibility();
+  } else {
+    if (value) {
+      console.log(value);
+      window.location.href = "./success-page";
+      email.value = "";
+    }
+  }
+});
+
+function errorVisibility() {
+  setTimeout(() => {
+    error.style.visibility = "hidden";
+  }, 2000);
+}
