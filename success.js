b3cc007953f7db3868cb0188b29ba5519fcf7c63
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2026-06-13 22:41:27 +0300

    replace the let with const

--- success.js
+++ success.js
@@ -1,4 +1,4 @@
-let dismiss_massage = document.querySelector("button");
+const dismiss_massage = document.querySelector("button");
 
 dismiss_massage.addEventListener("click", () => {
   window.location.href = "./index.html";
